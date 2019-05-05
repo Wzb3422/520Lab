d28b055f73807adb3ed92e394a29a6f91b079f7b
孙翔宇 <1339184537@qq.com>
2019-05-06 00:28:53 +0800

    :sparkles:feat(css)：添加了背景

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Routers from './Router'
 import GlobalReset from './style/reset'
 import { Provider } from 'react-redux'
 import store from './store'
+import { Grid,Imge} from './style/background'
 
 function App() {
   return (
     <Fragment>
+      <Grid/>
+      <Imge/>
       <Provider store={store}>
         <GlobalReset />
         <Routers />
